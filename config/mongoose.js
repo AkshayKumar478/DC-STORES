46cf2c88dfd845f143959e5042b1fe03b8211a65
AkshayKumar478 <iamakshay100@gmail.com>
2024-12-04 17:27:47 +0530

    hello

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const dbURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(dbURI)
+mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.log('Error connecting to MongoDB Atlas:', err));
