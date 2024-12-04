2782a3be371e43c5d83006e88141379cf9a79f84
AkshayKumar478 <iamakshay100@gmail.com>
2024-12-04 12:06:25 +0530

    updated index and package jason(installed mongodb)

--- index.js
+++ index.js
@@ -27,25 +27,6 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use((req,res,next)=>{
-//   if(!req.session.user){
-//     req.session.user={
-//       _id: '6733518a36201fe40adb5d73',
-//       name: 'Akshay Kumar',
-//       email: 'iamakshaykumar96@gmail.com',
-//       password: '$2a$10$.Ud7EnvQmrTgTdeGPi3lNeZHw2SkLs8CyMfpOiMOwjcbWk7DajdnK',
-//       isBlocked: false,
-//       walletBalance: 68571,
-//       wishList: [],
-//       createdAt: '2024-11-12T13:00:58.992Z',
-//       updatedAt: '2024-11-13T05:30:14.962Z',
-//       __v: 0
-//     }
-//   }
-  
-//   next()
-  
-// })
 
 app.use((req,res,next)=>{
   res.setHeader('cache-control','no-store')
