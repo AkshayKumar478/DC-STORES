56946ce4c979867af4d1d0d05b8196f6e7245a02
AkshayKumar478 <iamakshay100@gmail.com>
2024-12-05 10:33:48 +0530

    updated to google auth

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -25,7 +25,7 @@ passport.use(new LocalStrategy({ usernameField: 'email' },
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4247/user/auth/google/callback"
+    callbackURL: "http://dcstores.ddns.net:4247/user/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, done) {
     try {
--- models/userSchema.js
+++ models/userSchema.js
@@ -24,7 +24,6 @@ const userSchema=new mongoose.Schema({
     }]
 },{timestamps:true})
 
-//hash password
 userSchema.pre('save',async function(next) {
     if(!this.isModified('password')) return next();
     this.password=await bcrypt.hash(this.password,10)
