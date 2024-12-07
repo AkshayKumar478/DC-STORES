c066ab28d41a6d2aa5a0f09d578e592373b5ce99
AkshayKumar478 <iamakshay100@gmail.com>
2024-12-07 16:30:24 +0530

    Signup  page updated

--- middlewares/cartCounter.js
+++ middlewares/cartCounter.js
@@ -1,22 +1,16 @@
 const Cart=require('../models/userCart')
 
 const cartCountMiddleware = async (req, res, next) => {
+    res.locals.cartCount = 0; // Default value
     try {
-        res.locals.cartCount = 0;
-
         if (req.session && req.session.user) {
-            const cart = await Cart.findOne({ userId: req.session.user._id });
-            res.locals.cartCount = cart 
-                ? cart.items.reduce((total, item) => total + item.quantity, 0) 
-                : 0;
+            const cart = await Cart.findOne({ userId: req.session.userId });
+            res.locals.cartCount = cart ? cart.items.length : 0;
         }
-
-        next();
     } catch (error) {
-        console.error('Cart Count Middleware Error:', error);
-        res.locals.cartCount = 0;
-        next();
+        console.error('Cart count middleware error:', error);
     }
+    next();
 };
 
 module.exports=cartCountMiddleware;
